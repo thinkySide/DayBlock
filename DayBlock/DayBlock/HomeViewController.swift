e631606a5ed20180088416356587a9b4d200f181
thinkySide <eunlyuing@gmail.com>
2023-03-31 23:30:31 +0900

    [Feat] Removed Storyboard for Code Base Design

--- DayBlock/DayBlock/HomeViewController.swift
+++ DayBlock/DayBlock/HomeViewController.swift
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
 
 
