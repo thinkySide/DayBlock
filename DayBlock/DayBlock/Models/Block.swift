fd925dd65a8012db050726c22895bbe1bf8f2826
thinkySide <eunlyuing@gmail.com>
2023-04-03 20:51:37 +0900

    [Feat] Add Block, Group Model

---
+++ DayBlock/DayBlock/Models/Block.swift
@@ -0,0 +1,18 @@
+//
+//  Block.swift
+//  DayBlock
+//
+//  Created by 김민준 on 2023/04/03.
+//
+
+import UIKit
+
+struct Block {
+    
+    var label: String
+    var icon: UIImage
+    var output: Double
+    var color: UIColor
+    var group: Group
+    
+}
---
+++ DayBlock/DayBlock/Models/Group.swift
@@ -0,0 +1,15 @@
+//
+//  Group.swift
+//  DayBlock
+//
+//  Created by 김민준 on 2023/04/03.
+//
+
+import Foundation
+
+struct Group {
+    
+    var name: String
+    var list: [Block]?
+    
+}
