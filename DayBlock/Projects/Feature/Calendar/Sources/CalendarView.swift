c09808ec6b2b3204bbf679e338a0e261569b349c
thinkyside <eunlyuing@gmail.com>
2026-02-04 22:05:49 +0900

    CalendarHeader 구현

--- DayBlock/Projects/Feature/Calendar/Sources/CalendarView.swift
+++ DayBlock/Projects/Feature/Calendar/Sources/CalendarView.swift
@@ -19,7 +19,62 @@ public struct CalendarView: View {
     
     public var body: some View {
         VStack(spacing: 0) {
+            NavigationBar()
             
+            Header()
+                .padding(.horizontal, 20)
+            
+            Spacer()
+        }
+    }
+    
+    @ViewBuilder
+    private func Header() -> some View {
+        HStack {
+             Text("2023.08")
+                .brandFont(.poppins(.bold), 22)
+                .foregroundStyle(DesignSystem.Colors.gray900.swiftUIColor)
+            
+            Button {
+                
+            } label: {
+                Text("today")
+                    .brandFont(.poppins(.bold), 13)
+                    .foregroundStyle(DesignSystem.Colors.gray900.swiftUIColor)
+                    .frame(height: 26)
+                    .padding(.horizontal, 10)
+                    .background(DesignSystem.Colors.gray100.swiftUIColor)
+                    .clipShape(Capsule())
+            }
+            .padding(.leading, 8)
+            .scaleButton()
+            
+            Spacer()
+            
+            CalendarMonthIndicator()
+        }
+    }
+    
+    @ViewBuilder
+    private func CalendarMonthIndicator() -> some View {
+        HStack(spacing: 16) {
+            Button {
+                
+            } label: {
+                DesignSystem.Icons.arrowLeft.swiftUIImage
+                    .resizable()
+                    .frame(width: 24, height: 24)
+            }
+            .scaleButton()
+            
+            Button {
+                
+            } label: {
+                DesignSystem.Icons.arrowRight.swiftUIImage
+                    .resizable()
+                    .frame(width: 24, height: 24)
+            }
+            .scaleButton()
         }
     }
 }
